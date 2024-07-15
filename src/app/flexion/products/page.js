a17150c7c26944c22215c8f993736dcf432b3f09
daimessdn@gmail.com <daimessdn@gmail.com>
2024-07-15 15:39:30 +0700

    updated product dashboard interface

--- src/app/flexion/products/page.js
+++ src/app/flexion/products/page.js
@@ -1,15 +1,130 @@
+"use client";
+
+import { useState } from "react";
+import { BiDollar } from "react-icons/bi";
+
+import { ligaSans } from "@/config/fonts.config";
+
 import Row from "@/components/Row";
 
 import AdminSidebar from "@/components/flexion/AdminSidebar";
 import ProductDashboard from "@/sections/flexion/products/ProductDashboard";
 
 export default function ProductPage() {
+  const [isCreatePopupOpen, setIsCreatePopupOpen] = useState(false);
+
   return (
     <section>
       <Row>
         <AdminSidebar />
 
-        <ProductDashboard />
+        <ProductDashboard
+          addProduct={() => {
+            setIsCreatePopupOpen(true);
+          }}
+        />
+
+        <div
+          className={
+            "fixed top-0 left-0 w-screen h-screen bg-gray/50 " +
+            (isCreatePopupOpen ? "flex" : "hidden")
+          }
+        >
+          <div
+            className={
+              "w-1/2 m-auto bg-white p-4 mt-4 shadow-md rounded-md transition duration-300 ease-in-out " +
+              (isCreatePopupOpen ? "opacity-100" : "opacity-0")
+            }
+          >
+            <h2 className={ligaSans.className + " text-3xl"}>Add Product</h2>
+
+            <form>
+              <div className="flex flex-col mt-4 gap-2">
+                <label className="text-gray-500" htmlFor="name">
+                  Product name
+                </label>
+
+                <input
+                  className="border border-gray-300 rounded-md p-2"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Input product name"
+                  required
+                />
+              </div>
+
+              <div className="flex flex-col mt-4 gap-2">
+                <label className="text-gray-500" htmlFor="description">
+                  Product description
+                </label>
+
+                <textarea
+                  className="border border-gray-300 rounded-md p-2 resize-none"
+                  id="description"
+                  name="description"
+                  rows={4}
+                  placeholder="Input product description"
+                ></textarea>
+              </div>
+
+              <div className="flex flex-col mt-4 gap-2">
+                <label className="text-gray-500" htmlFor="price">
+                  Product price
+                </label>
+
+                <div className="flex items-center">
+                  <label
+                    htmlFor="price"
+                    className="border border-gray bg-gray text-white p-2 rounded-l-md"
+                  >
+                    <BiDollar size={24} />
+                  </label>
+                  <input
+                    className="border border-gray-300 rounded-r-md p-2"
+                    type="number"
+                    placeholder="0.00"
+                    name="price"
+                    id="price"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col mt-4 gap-2">
+                <label className="text-gray-500" htmlFor="image">
+                  Product image
+                </label>
+
+                <input
+                  className="border border-gray-300 rounded-md p-2"
+                  placeholder="Upload your product image"
+                  type="file"
+                  id="image"
+                  name="image"
+                />
+              </div>
+
+              <div className="flex flex-row gap-2 items-center">
+                <button
+                  className="mt-4 px-4 py-2 rounded-full shadow-md bg-secondary text-white text-sm block flex items-center gap-2"
+                  href="/flexion/products/edit"
+                >
+                  Add product
+                </button>
+
+                <button
+                  className="mt-4 px-4 py-2 rounded-full shadow-md bg-white text-secondary text-sm block flex items-center gap-2"
+                  onClick={() => {
+                    setIsCreatePopupOpen(false);
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </Row>
     </section>
   );
--- src/sections/flexion/products/ProductDashboard.js
+++ src/sections/flexion/products/ProductDashboard.js
@@ -4,13 +4,20 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { useEffect, useState } from "react";
+import { BiDollar } from "react-icons/bi";
 
 import { ligaSans } from "@/config/fonts.config";
 
 import Column from "@/components/Column";
 
-export default function ProductDashboard() {
+export default function ProductDashboard({ addProduct }) {
   const [products, setProducts] = useState([]);
+
+  const [productUpdated, setProductUpdated] = useState({});
+  const [productDeleted, setProductDeleted] = useState({});
+
+  const [isEditPopupOpen, setIsEditPopupOpen] = useState(false);
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:3000/api/products")
@@ -18,68 +25,257 @@ export default function ProductDashboard() {
       .then((res) => setProducts(res.data));
   }, []);
 
-  const handleLoadProducts = () => {};
+  const handleEditPopup = (id) => {
+    const productBeingUpdated = products.find((p) => p.id === id);
+    setProductUpdated(productBeingUpdated);
+    setIsEditPopupOpen(true);
+  };
+
+  const handleDeletePopup = (id) => {
+    const productBeingDeleted = products.find((p) => p.id === id);
+    setProductDeleted(productBeingDeleted);
+    setIsDeletePopupOpen(true);
+  };
 
   return (
-    <Column className="p-4">
-      <h1 className={ligaSans.className + " text-3xl mb-4"}>Flex Products</h1>
+    <>
+      <Column className="p-4">
+        <h1 className={ligaSans.className + " text-3xl mb-4"}>Flex Products</h1>
 
-      <Link
-        className="uppercase px-4 py-2 rounded-2xl bg-secondary text-white flex items-center gap-2 w-fit text-xs mb-4"
-        href="/flexion/products/add"
+        <button
+          className="uppercase px-4 py-2 rounded-full bg-secondary text-white flex items-center gap-2 w-fit text-sm mb-4"
+          onClick={addProduct}
+        >
+          Add Products
+        </button>
+
+        <div className="rounded-lg overflow-hidden w-full">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-green-100">
+                <th className="p-2 w-1/4">Name</th>
+                <th className="p-2 w-1/2">Description</th>
+                <th>Price</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {products.map((p) => (
+                <tr key={p.id} className="odd:bg-green-50 even:bg-green-100">
+                  <td className="p-2">
+                    <div className="flex flex-row gap-2 items-center">
+                      <Image
+                        src={p.imageUrl}
+                        alt={p.description}
+                        width={64}
+                        height={64}
+                      />
+
+                      {p.name}
+                    </div>
+                  </td>
+
+                  <td className="p-2 w-1/2">{p.description}</td>
+
+                  <td className="p-2 w-1/8">${p.price}</td>
+
+                  <td className="p-2">
+                    <div className="flex flex-row gap-2 items-center">
+                      <button
+                        className="uppercase px-4 py-2 rounded-full bg-secondary text-white inline-block text-sm flex items-center gap-2"
+                        onClick={() => {
+                          handleEditPopup(p.id);
+                        }}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="uppercase px-4 py-2 rounded-full bg-secondary text-white inline-block text-sm flex items-center gap-2"
+                        onClick={() => {
+                          handleDeletePopup(p.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </Column>
+
+      <div
+        className={
+          "fixed top-0 left-0 w-screen h-screen bg-gray/50 " +
+          (isEditPopupOpen ? "flex" : "hidden")
+        }
       >
-        Add Products
-      </Link>
+        <div
+          className={
+            "w-1/2 m-auto bg-white p-4 mt-4 shadow-md rounded-md transition duration-300 ease-in-out " +
+            (isEditPopupOpen ? "opacity-100" : "opacity-0")
+          }
+        >
+          <h2 className={ligaSans.className + " text-3xl"}>Edit Product</h2>
 
-      <table className="border-collapse">
-        <thead>
-          <tr className="border border-primary">
-            <th className="p-2 w-1/4"></th>
-            <th className="p-2 w-1/4">Name</th>
-            <th className="p-2 w-1/8">Description</th>
-            <th>Price</th>
-            <th></th>
-          </tr>
-        </thead>
+          <blockquote className="bg-secondary text-white p-2 shadow-md rounded-md mt-4">
+            You will edit "{productUpdated.name || ""}".
+          </blockquote>
 
-        <tbody>
-          {products.map((p) => (
-            <tr className="border border-primary align-middle h-full">
-              <td className="p-2 w-[10%]">
-                <Image
-                  src={p.imageUrl}
-                  alt={p.description}
-                  width={50}
-                  height={50}
+          <form>
+            <div className="flex flex-col mt-4 gap-2">
+              <label className="text-gray-100" htmlFor="name">
+                Product name
+              </label>
+
+              <input
+                className="border border-gray-100 rounded-md p-2"
+                type="text"
+                id="name"
+                name="name"
+                value={productUpdated.name}
+                onChange={(e) => {
+                  setProductUpdated({
+                    ...productUpdated,
+                    name: e.target.value,
+                  });
+                }}
+                placeholder="Input product name"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col mt-4 gap-2">
+              <label className="text-gray-100" htmlFor="description">
+                Product description
+              </label>
+
+              <textarea
+                className="border border-gray-100 rounded-md p-2 resize-none"
+                id="description"
+                name="description"
+                value={productUpdated.description}
+                onChange={(e) => {
+                  setProductUpdated({
+                    ...productUpdated,
+                    description: e.target.value,
+                  });
+                }}
+                rows={4}
+                placeholder="Input product description"
+              ></textarea>
+            </div>
+
+            <div className="flex flex-col mt-4 gap-2">
+              <label className="text-gray-100" htmlFor="price">
+                Product price
+              </label>
+
+              <div className="flex items-center">
+                <label
+                  htmlFor="price"
+                  className="border border-gray bg-gray text-white p-2 rounded-l-md"
+                >
+                  <BiDollar size={24} />
+                </label>
+
+                <input
+                  className="border border-gray-100 rounded-r-md p-2"
+                  type="number"
+                  placeholder="0.00"
+                  name="price"
+                  value={productUpdated.price}
+                  onChange={(e) => {
+                    setProductUpdated({
+                      ...productUpdated,
+                      price: e.target.value,
+                    });
+                  }}
+                  id="price"
+                  required
                 />
-              </td>
+              </div>
+            </div>
 
-              <td className="p-2 w-1/4">{p.name}</td>
+            <div className="flex flex-col mt-4 gap-2">
+              <label className="text-gray-100" htmlFor="image">
+                Product image
+              </label>
 
-              <td className="p-2 w-1/4">{p.description}</td>
+              <input
+                className="border border-gray-100 rounded-md p-2"
+                placeholder="Upload your product image"
+                type="file"
+                id="image"
+                name="image"
+              />
+            </div>
 
-              <td className="p-2 w-1/8">
-                <p className="grow-1">${p.price}</p>
-              </td>
+            <div className="flex flex-row gap-2 items-center">
+              <button
+                className="mt-4 px-4 py-2 rounded-full shadow-md bg-secondary text-white text-sm block flex items-center gap-2"
+                href="/flexion/products/edit"
+              >
+                Edit product
+              </button>
 
-              <td className="py-4 p-2 items-center flex gap-2 min-h-full">
-                <Link
-                  className="uppercase px-4 py-2 rounded-2xl bg-secondary text-white block text-xs flex items-center gap-2"
-                  href="/flexion/products/edit"
-                >
-                  Edit
-                </Link>
-                <Link
-                  className="uppercase px-4 py-2 rounded-2xl bg-secondary text-white block text-xs flex items-center gap-2"
-                  href="/flexion/products/delete"
-                >
-                  Delete
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </Column>
+              <button
+                className="mt-4 px-4 py-2 rounded-full shadow-md bg-white text-secondary text-sm block flex items-center gap-2"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsEditPopupOpen(false);
+                  setProductUpdated({});
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div
+        className={
+          "fixed top-0 left-0 w-screen h-screen bg-gray/50 " +
+          (isDeletePopupOpen ? "flex" : "hidden")
+        }
+      >
+        <div
+          className={
+            "w-1/2 m-auto bg-white p-4 mt-4 shadow-md rounded-md transition duration-300 ease-in-out " +
+            (isDeletePopupOpen ? "opacity-100" : "opacity-0")
+          }
+        >
+          <h2 className={ligaSans.className + " text-3xl"}>Delete Product</h2>
+
+          <p className="mt-4">
+            Are you sure want to delete "{productDeleted.name || ""}".
+          </p>
+
+          <div className="flex flex-row gap-2 items-center">
+            <button
+              className="mt-4 px-4 py-2 rounded-full shadow-md bg-secondary text-white text-sm block flex items-center gap-2"
+              href="/flexion/products/delete"
+            >
+              Delete product
+            </button>
+
+            <button
+              className="mt-4 px-4 py-2 rounded-full shadow-md bg-white text-secondary text-sm block flex items-center gap-2"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsDeletePopupOpen(false);
+                setProductDeleted({});
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
